d0227dae711b8cdf519db4353813dfafae1928e1
eivsal <eivind.salvsn@gmail.com>
2017-03-01 15:48:01 +0100

    endret main

--- main.c
+++ main.c
@@ -10,16 +10,41 @@ int main() {
     }
 
     printf("Press STOP button to stop elevator and exit program.\n");
-
+    
+    
     elev_set_motor_direction(DIRN_UP);
 
     while (1) {
-        // Change direction when we reach top/bottom floor
-        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
-            elev_set_motor_direction(DIRN_DOWN);
-        } else if (elev_get_floor_sensor_signal() == 0) {
-            elev_set_motor_direction(DIRN_UP);
+        /*
+        if (elev_get_stop_signal() == 100){                 //usikker på hva som kommer ut av io read bits
+            fsm_stopButtonPressed();
+            while(elev_get_stop_signal() == 100){
+                continue;
+            }
+            fsm_stopButtonUnpressed();
         }
+        
+        */
+        
+        for(int i = 0; i< 3; i++){
+            elev_button_type_t button = i;
+            for(int floor= 0; floor< 4; j++){
+                
+                if(elev_get_button_signal(button, floor) == 1){
+                    fsm_buttonIsPushed(button,floor);
+                    elev_set_button_lamp(button,floor);
+                }
+            }
+        }
+        
+        if (elev_get_floor_sensor_signal() != -1){
+            fsm_arrivedatfloor(elev_get_floor_sensor_signal());
+        }
+        
+        if(timer_isTimeOut() == 1){
+            fsm_timeOut();
+        }
+        
 
         // Stop elevator and exit program if the stop button is pressed
         if (elev_get_stop_signal()) {
